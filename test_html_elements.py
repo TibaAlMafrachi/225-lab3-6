23796a5b633aaabbd352498c1f643f093a3a6a7d
TibaAlMafrachi <almafrtk@miamioh.edu>
2026-04-02 14:02:45 -0400

    Update test_html_elements.py

--- test_html_elements.py
+++ test_html_elements.py
@@ -6,21 +6,23 @@ import unittest
 
 class TestH5Tag(unittest.TestCase):
     def setUp(self):
-        # Setup Firefox options
         firefox_options = Options()
-        firefox_options.add_argument("--headless")  # Ensures the browser window does not open
+        firefox_options.add_argument("--headless")
         firefox_options.add_argument("--no-sandbox")
         firefox_options.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Firefox(options=firefox_options)
 
     def test_h5_tag_content(self):
         driver = self.driver
-        driver.get("http://jenkins-225.cit.regionals.miamioh.edu/almafrtk6/")  # Replace with your target website
-        
-        # Locate the <h5> tag and get its text
-        h5_text = driver.find_element(By.TAG_NAME, "h5").text
-        
-        # Assert that the text of the <h5> tag is "Lab 3-6 Works!"
+        driver.get("http://jenkins-225.cit.regionals.miamioh.edu/almafrtk6/")
+
+        from selenium.webdriver.support.ui import WebDriverWait
+        from selenium.webdriver.support import expected_conditions as EC
+
+        wait = WebDriverWait(driver, 10)
+        h5_element = wait.until(EC.presence_of_element_located((By.TAG_NAME, "h5")))
+        h5_text = h5_element.text
+
         self.assertEqual("Lab 3-6 Works!", h5_text, "The <h5> tag does not contain the text 'Lab 3-6 Works!'")
 
     def tearDown(self):
